18872a5c3ca5cea38747c24fafa775259be0410e
Joerg Schaarschmidt <joerg.schaarschmidt@kit.edu>
2021-08-06 10:54:10 +0200

    Update gloassary.py to improve functiona:

--- glossary.py
+++ glossary.py
@@ -1,70 +1,90 @@
-#%%
 import json
 import re
+import string
+from collections import OrderedDict
+
 from markdown import Markdown
 
 
-
-#%%
 class Term(dict):
-   id = None
+    id = None
 
-   def __init__(self, id="") -> None:
-       super().__init__()
-       self.id = id
+    def __init__(self, *args, key="", **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.id = key
 
-   def set_item(self, item, data, lang='de', append=False):
-       if lang not in self:
-           self[lang] = {}
-       if append and item in self[lang]:
-           self[lang][item] += f" {data}"
-       else:
-           self[lang][item] = data
-   
-   def merge(self, term):
-      for k, v in term.items():
-         if k not in self:
-            self[k] = v
-   
-   
+    def set_item(self, item, data, lang='de', append=False):
+        if lang not in self:
+            self[lang] = {}
+        if append and item in self[lang]:
+            self[lang][item] += f" {data}"
+        else:
+            self[lang][item] = data
 
-#%%
+    def merge(self, term):
+        for k, v in term.items():
+            if k not in self:
+                self[k] = v
+
+
 class Glossary(dict):
-   DEF_RE = re.compile(r'^[ ]{0,3}\[[ ]*id=([^\]]*)[ ]*\]')
-   md = Markdown()
+    DEF_RE = re.compile(r'^[ ]{0,3}\[[ ]*id=([^\]]*)[ ]*\]')
+    localized_glossaries = {}
 
-   def parse_markdown(self, filename, lang="de"):
-      with open(filename, "r", encoding="utf-8") as fh:
-         lines = fh.readlines()
-         etree = self.md.parser.parseDocument(lines)
-      terms = []
-      current_term = None
-      for item in etree.iter():
+    def __init__(self, json_file=None):
+        super(Glossary, self).__init__()
+        if json_file:
+            self.parse_json(json_file)
 
-         if item.tag == 'h2':
-            current_term = Term()
-            terms.append(current_term)
-            current_term.set_item('term', item.text.strip(), lang=lang)
-         
-         elif item.tag == 'p' and current_term:
-            m = self.DEF_RE.search(item.text)
-            if m:
-                current_term.id = m.groups()[0].strip()
+    def get_localized_glossary(self, lang):
+        glossary = OrderedDict((c, {}) for c in string.ascii_uppercase)
+        for t in self.values():
+            if lang in t:
+                term = t[lang]['term']
+                glossary[term[0].upper()][term] = t[lang]['definition']
+        return glossary
+
+    def parse_markdown(self, filename, lang="de"):
+        md = Markdown()
+        with open(filename, "r", encoding="utf-8") as fh:
+            lines = fh.readlines()
+            etree = md.parser.parseDocument(lines)
+        terms = []
+        current_term = None
+        for item in etree.iter():
+
+            if item.tag == 'h2':
+                current_term = Term()
+                terms.append(current_term)
+                current_term.set_item('term', item.text.strip(), lang=lang)
+
+            elif item.tag == 'p' and current_term:
+                m = self.DEF_RE.search(item.text)
+                if m:
+                    current_term.id = m.groups()[0].strip()
+                else:
+                    current_term.set_item('definition', item.text, append=True, lang=lang)
+
+        for t in terms:
+            if t.id in self:
+                self[t.id].merge(t)
             else:
-               current_term.set_item('definition', item.text, append=True, lang=lang)
-      
-      for t in terms:
-         if t.id in self:
-            self[t.id].merge(t)
-         else:
-            self[t.id] = t
+                self[t.id] = t
+
+    def parse_json(self, filename):
+        with open(filename) as fh:
+            raw_data = json.load(fh)
+        languages = set()
+        for k, v in raw_data.items():
+            self[k] = Term(key=k, **v)
+            languages.update(v.keys())
+        for l in languages:
+            self.localized_glossaries[l] = self.get_localized_glossary(l)
 
 
-# %%
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     g = Glossary()
     g.parse_markdown('Glossar.md')
     g.parse_markdown('Glossary.md', lang='en')
     with open('glossary.json', 'w') as fh:
-       json.dump({k: g[k] for k in sorted(g)}, fh, indent=2)
+        json.dump({k: g[k] for k in sorted(g)}, fh, indent=2)
